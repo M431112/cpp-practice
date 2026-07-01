d478562100c4edae7971b161ce31008ad413ccd7
Bob <atif24into7@gmail.com>
2026-07-02 01:11:45 +0530

    learned datatypes

---
+++ datatypes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+/*Dta types in C++ is catagorised in three types:
+  - Built-in (int, float, char, double, bool) 
+  - User-defined (Struct, Union, Enum) -> see these later
+  - Derived (Array, Function, Pointer) -> see these later*/
+  
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 4;
+    float c = 5.67;
+    float d = 8.45;
+    char bob = 'm';
+    double pi = 3.14159265359;
+    bool gay = true;
+
+    cout<<"The sum of a and b is "<<a+b<<".\nThe multiplication of c and d is "<<c*d;
+    cout<<".\nThe codeword for bob is "<<bob;
+    cout<<".\nThe value of pi is "<<pi<<".\nIs the reader gay? "<<gay<<".\n(1 means true, 0 means false)";
+
+    return 0;
+
+}
