aa3d28454f76b04e7245715dbd0c203026c3b093
Bob <atif24into7@gmail.com>
2026-07-01 19:55:31 +0530

    Added Day 1 C++ programs

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+// This line iss commented out
+
+/*this 
+is a 
+multi-line
+comment*/
+
+using namespace std;
+int main(){
+    int sum = 6; // as a example(sum)
+    cout<<"Hello World  " <<sum;
+    return 0;
+} 
