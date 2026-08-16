92e920ba3cdcf37b5a1b5d89f8e2cd18eeb7299e
Bob <atif24into7@gmail.com>
2026-08-16 23:16:30 +0530

    Learn function objects and their usage

---
+++ 43_function_objects_functor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+// A function object is a function wrapped in a class so that it is available as an object. That is, we can then use a function as an object.
+
+int main()
+{
+    int arr[] = {45, 78, 3, 5, 9, 2};
+
+    // sort(arr, arr+5); // Last element will be untouched!
+    // sort(arr, arr+4);  // Last two element will be untouched!
+
+    cout << "Array in descending order:\n";
+    sort(arr, arr + 6, greater<int>());
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    // For more function objects, visit cppreference-algorithm!
+    return 0;
+}
